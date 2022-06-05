609382580cfda4a2c71aa88799235ce5275fba45
LoloxDev <loris_labarre@outlook.fr>
2022-06-05 15:23:07 +0200

    avancement formulaire

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 // Run FULLSCREEN
 
-new fullpage('#fullpage', {
-	ancres :['firstPage','secondPage','thirdPage']
-  });
+new fullpage("#fullpage", {
+    ancres: ["firstPage", "secondPage", "thirdPage"],
+});
 
-
-  function openForm() {
+function openForm() {
     document.getElementById("popupForm").style.display = "block";
     document.getElementById("openBtn").style.zIndex = "-1";
-  }
+    document.getElementById("community").style.zIndex = "-1";
+}
 
-  function closeForm() {
+function closeForm() {
     document.getElementById("popupForm").style.display = "none";
     document.getElementById("openBtn").style.zIndex = "1";
-  }
+    document.getElementById("community").style.zIndex = "1";
+}
