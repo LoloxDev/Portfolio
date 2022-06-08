c0b69bd42e17e2748fbd635f824563ed47eaaaf1
LoloxDev <loris_labarre@outlook.fr>
2022-06-08 22:21:51 +0200

    Pixel perfect

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ new fullpage("#fullpage", {
     navigation: true,
     anchors: ['section1', 'section2', 'section3', 'section4', 'section5'],
     fixedElements:'.nav',
+    navigationTooltips: ['Acceuil','Projets','Parcours','Compétences','Présentation'],
+    showActiveTooltip: true,
 });
 
 function openForm() {
