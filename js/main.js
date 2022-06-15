29c05b5b213849589c97f3973a525edaecf257b1
loloxdev <loris_labarre@outlook.fr>
2022-06-15 09:42:00 +0200

    Mise en place du loader

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ new fullpage("#fullpage", {
     autoScrolling: true,
     navigation: true,
     anchors: ['section1', 'section2', 'section3', 'section4', 'section5'],
-    fixedElements:'.nav, #loader, .btn',
+    fixedElements:'.nav, #loader',
     navigationTooltips: ['Acceuil','Projets','Parcours','Compétences','Présentation'],
     showActiveTooltip: true,
     navigationPosition:true,
